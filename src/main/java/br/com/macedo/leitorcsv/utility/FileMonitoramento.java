0851cad8e05bf1ecbff54fb28c744a22bd21744c
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-04-26 10:34:48 -0400

    Adicionado metodos para recebimento de uma Lista de Alunos, e persistencia e movimentação de arquivo.

--- src/main/java/br/com/macedo/leitorcsv/utility/FileMonitoramento.java
+++ src/main/java/br/com/macedo/leitorcsv/utility/FileMonitoramento.java
@@ -1,6 +1,8 @@
 package br.com.macedo.leitorcsv.utility;
 
+import br.com.macedo.leitorcsv.entity.Aluno;
 import br.com.macedo.leitorcsv.mapper.ConversorCsvToEntityMapper;
+import br.com.macedo.leitorcsv.service.CsvService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 @Component("fileMonitoramentoBuscaCsv")
 public class FileMonitoramento {
@@ -18,7 +21,9 @@ public class FileMonitoramento {
     @Autowired
     ConversorCsvToEntityMapper csvToEntityMapper;
     @Autowired
-    CsvToMultipart csvToMultipart;
+    CsvService csvService;
+    @Autowired
+    FileMover fileMover;
     private final static Logger LOGGER = LoggerFactory.getLogger(FileMonitoramento.class);
 
 
@@ -26,23 +31,25 @@ public class FileMonitoramento {
     private void buscaCsv() {
 
 
-        File folderPath = new File(("data/arquivorecebido/"));
+        File folderPath = new File(("data/arquivosrecebidos/"));
 
         LOGGER.info("Buscando arquivo CSV");
 
         File[] arquivos = folderPath.listFiles((dir, name) -> name.toLowerCase().endsWith(".csv"));
 
         if(arquivos != null){
-        Arrays.stream(arquivos).forEach(arquivo -> {
+            Arrays.stream(arquivos).forEach(arquivo -> {
                 LOGGER.info("Arquivo encontrado: " + arquivo.getName());
-            try {
-                MultipartFile multipartFile = csvToMultipart.CsvToMultipartFile(arquivo);
-                csvToEntityMapper.converterCsvParaEntity(multipartFile);
-                LOGGER.info("Arquivo convertido em entidade!");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+                try {
+
+                    List<Aluno> registros = csvService.lerCsv(arquivo.getAbsolutePath());
+                    csvService.persistirEntidades(registros);
+                    fileMover.moverArquivo(arquivo);
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         }
     }
 
