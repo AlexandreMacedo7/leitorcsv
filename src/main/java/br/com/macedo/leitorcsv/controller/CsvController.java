0b6a92ef2a1ce9aed0d80a88fca01bc2363040c1
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-04-26 10:31:04 -0400

    Alteração de importações

--- src/main/java/br/com/macedo/leitorcsv/controller/CsvController.java
+++ src/main/java/br/com/macedo/leitorcsv/controller/CsvController.java
@@ -1,19 +1,15 @@
 package br.com.macedo.leitorcsv.controller;
 
-import br.com.macedo.leitorcsv.dto.RegistroAlunoDTO;
-import br.com.macedo.leitorcsv.entity.RegistroAluno;
 import br.com.macedo.leitorcsv.service.CsvService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 public class CsvController {
